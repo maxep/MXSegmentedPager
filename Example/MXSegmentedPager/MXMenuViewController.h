00e4c3c3a38d0e7255787d4dc8c5e258217ec06e
Maxime Epain <maime.epain@gmail.com>
2015-03-05 23:01:32 +0100

    Handle UICollectionView scrolling. Clean up example.

---
+++ Example/MXSegmentedPager/MXMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MXMenuViewController.h
+//  MXSegmentedPager
+//
+//  Created by Maxime Epain on 05/03/2015.
+//  Copyright (c) 2015 Maxime Epain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXMenuViewController : UIViewController
+
+@end
---
+++ Example/MXSegmentedPager/MXMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  MXMenuViewController.m
+//  MXSegmentedPager
+//
+//  Created by Maxime Epain on 05/03/2015.
+//  Copyright (c) 2015 Maxime Epain. All rights reserved.
+//
+
+#import "MXMenuViewController.h"
+
+@interface MXMenuViewController ()
+
+@end
+
+@implementation MXMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
