5bbd9518901517cfd5c66971677112fbca74c483
Maxime Epain <maxime.epain@gmail.com>
2015-03-06 17:07:21 +0100

    Fix Example

--- Example/MXSegmentedPager/MXCollectionViewController.m
+++ Example/MXSegmentedPager/MXCollectionViewController.m
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-#import "MXFancyViewController.h"
+#import "MXCollectionViewController.h"
 #import "UICollectionView+MXSegmentedPager.h"
 
 @interface MXFancyViewController () <MXSegmentedPagerDelegate, MXSegmentedPagerDataSource, UITableViewDelegate, UITableViewDataSource>
--- Example/MXSegmentedPager/MXSimpleViewController.m
+++ Example/MXSegmentedPager/MXSimpleViewController.m
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "MXViewController.h"
+#import "MXSimpleViewController.h"
 #import "MXSegmentedPager.h"
 
 @interface MXViewController () <MXSegmentedPagerDelegate, MXSegmentedPagerDataSource, UITableViewDelegate, UITableViewDataSource>
